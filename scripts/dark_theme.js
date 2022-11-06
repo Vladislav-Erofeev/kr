b61791aaad4636befb1e92122a67e504726977bc
Vladislav <92443812+Vladislav-Erofeev@users.noreply.github.com>
2022-11-06 20:25:20 +0300

    Добавлены страницы всех остальных учёных

---
+++ scripts/dark_theme.js
@@ -0,0 +1,30 @@
+var btn1 = document.getElementById("theme_btn1");
+var btn2 = document.getElementById("theme_btn2");
+var link = document.getElementById("theme_link");
+
+btn1.addEventListener("click", function () { ChangeTheme(); });
+btn2.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "style/light.css";
+    let darkTheme = "style/dark.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+        currTheme = darkTheme;
+        theme = "dark";
+    }
+    else
+    {
+        currTheme = lightTheme;
+        theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
