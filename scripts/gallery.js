2b6a0981085279ce95c608d50bc2122f604415f2
Vladislav <92443812+Vladislav-Erofeev@users.noreply.github.com>
2022-11-16 15:46:02 +0300

    Настроен вид галереи, добавлен скрипт

---
+++ scripts/gallery.js
@@ -0,0 +1,21 @@
+let field = document.getElementById('scroller');
+let leftPos = 0;
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+left.addEventListener('click', () => {
+    if (leftPos == 0) {
+        leftPos = -1500;
+    } else {
+        leftPos += 500;
+    }
+    field.style.left = `${leftPos}px`;
+})
+
+right.addEventListener('click', () => {
+    if (leftPos == -1500) {
+        leftPos = 0;
+    } else {
+        leftPos -= 500;
+    }
+    field.style.left = `${leftPos}px`;
+})
