569381a06f754376d32d74a80ad36a9573a1f551
Vladislav <92443812+Vladislav-Erofeev@users.noreply.github.com>
2022-11-28 11:56:25 +0300

    Сделана галерея для всех страниц

--- scripts/science.js
+++ scripts/science.js
@@ -1,26 +1,22 @@
-function myapp() {
-    const buttons = document.querySelectorAll(".button");
-    const cards = document.querySelectorAll(".card");
+const buttons = document.querySelectorAll(".button");
+const cards = document.querySelectorAll(".card");
 
-    function filter(category, items) {
-        items.forEach((item) => {
-            const isItemFiltered = !item.classList.contains(category);
-            const isShowAll = category.toLowerCase() === "all";
-            if (isItemFiltered && !isShowAll) {
-                item.classList.add("hide");
-            } else {
-                item.classList.remove("hide");
-            }
-        });
-    }
-
-    buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-            const currentCategory = button.dataset.filter;
-            console.log(currentCategory);
-            filter(currentCategory, cards);
-        });
+function filter(category, items) {
+    items.forEach((item) => {
+        const isItemFiltered = !item.classList.contains(category);
+        const isShowAll = category.toLowerCase() === "all";
+        if (isItemFiltered && !isShowAll) {
+            item.classList.add("hide");
+        } else {
+            item.classList.remove("hide");
+        }
     });
 }
 
-myapp();
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const currentCategory = button.dataset.filter;
+        console.log(currentCategory);
+        filter(currentCategory, cards);
+    });
+});
